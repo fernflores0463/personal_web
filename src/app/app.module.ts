a8ae44de502736f9478773313d2039104a51330a
Fernando Flores-Hernandez <elfleur@fernandos-mbp.wireless.rit.edu>
2017-12-06 01:54:55 -0500

    added an iframe with calendar in it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MdButtonModule, MdCardModule, MdListModule, MdMenuModule, MdToolbarModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdButtonModule,
+    MdToolbarModule,
+    MdCardModule,
+    MdMenuModule,
+    MdListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
